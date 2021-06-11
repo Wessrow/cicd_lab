1d8d543c024d6ac8035cf6748c36ab6a2fb794f5
Wessrow <gurra_gla@hotmail.com>
2021-06-11 15:31:42 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+CI/CD Lab  - By Gustav Larsson
+Written as part of DEVCOR Studies
+"""
+
+def add(x,y):
+    """ Simple adding """
+    return x+y
+
+def subtract(x,y):
+    """ Simple subtration """
+    return x-y
+
+if __name__ == "__main__":
+    """ Run this if not imported """
+    pass
